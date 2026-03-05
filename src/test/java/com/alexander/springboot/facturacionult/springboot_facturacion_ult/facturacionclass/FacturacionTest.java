3052b80c645fec069aa94cb19c339db648d2e2a4
Alexander Lopez <lopez.panta.alexander@gmail.com>
2026-03-05 18:13:53 -0500

    nuevos archivos y clases del test

---
+++ src/test/java/com/alexander/springboot/facturacionult/springboot_facturacion_ult/facturacionclass/FacturacionTest.java
@@ -0,0 +1,10 @@
+package com.alexander.springboot.facturacionult.springboot_facturacion_ult.facturacionclass;
+
+import com.intuit.karate.junit5.Karate;
+
+class FacturacionTest {
+    @Karate.Test
+    Karate testFacturas() {
+        return Karate.run("facturacion/factura").relativeTo(getClass());
+    }
+}
